7886b4b1d39d54d92d989bc0a37998bfa4336a96
Stepan Koryagin <stepanswim@gmail.com>
2023-06-24 19:04:57 +0300

    made the implementation of the authorization service

--- src/main/java/com/example/tasklist/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/tasklist/service/impl/AuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.tasklist.service.impl;
 
+import com.example.tasklist.domain.user.User;
 import com.example.tasklist.service.AuthService;
 import com.example.tasklist.service.UserService;
 import com.example.tasklist.web.dto.auth.JwtRequest;
@@ -7,6 +8,7 @@ import com.example.tasklist.web.dto.auth.JwtResponse;
 import com.example.tasklist.web.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,11 +21,33 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public JwtResponse login(JwtRequest loginRequest) {
-        return null;
+        //redirecting the Spring to the method JwtUserDetailsService
+       JwtResponse jwtResponse = new JwtResponse();
+       authenticationManager.authenticate(
+               new UsernamePasswordAuthenticationToken(
+                       loginRequest.getUsername(),
+                       loginRequest.getPassword())
+       );
+       User user = userService.getByUsername(loginRequest.getUsername());
+       jwtResponse.setId(user.getId());
+       jwtResponse.setUsername(user.getUsername());
+       jwtResponse.setAccessToken(
+               jwtTokenProvider.createAccessToken(
+                       user.getId(),
+                       user.getUsername(),
+                       user.getRoles())
+       );
+       jwtResponse.setRefreshToken(
+               jwtTokenProvider.createRefreshToken(
+                       user.getId(),
+                       user.getUsername())
+       );
+        return jwtResponse;
     }
 
     @Override
     public JwtResponse refresh(String refreshToken) {
-        return null;
+
+        return jwtTokenProvider.refreshUserTokens(refreshToken);
     }
 }
